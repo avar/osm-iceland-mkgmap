65c8969ff41f191bd7d3eda0acc1a760e5004ac5
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-01-18 10:27:16 +0000

    nice(1) the db import to 5

--- script/daily-database-import.sh
+++ script/daily-database-import.sh
@@ -29,11 +29,11 @@ echo "  host: localhost"     >> config/database.yml
 echo "  encoding: utf8"      >> config/database.yml
 
 # migrate!
-rake db:migrate
+nice -n 5 rake db:migrate
 
 # Import Iceland.osm
 echo Importing data
-osmosis --read-xml-0.6 /var/www/osm.nix.is/latest/Iceland.osm.bz2 --write-apidb-0.6 populateCurrentTables=yes host="localhost" database="osmistmp" user="osmistmp" password="osmistmp" validateSchemaVersion=no
+nice -n 5 osmosis --read-xml-0.6 /var/www/osm.nix.is/latest/Iceland.osm.bz2 --write-apidb-0.6 populateCurrentTables=yes host="localhost" database="osmistmp" user="osmistmp" password="osmistmp" validateSchemaVersion=no
 
 ## Rename it & delete
 

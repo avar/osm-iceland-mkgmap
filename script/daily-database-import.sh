eb1bb42bb9a8fa1207b49c99fd368ccd8ee1a143
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-02-04 11:36:42 +0000

    experimented with a pure osmosis import, didn't work due to user id = -1 error

--- script/daily-database-import.sh
+++ script/daily-database-import.sh
@@ -18,10 +18,12 @@ echo Creating user/db
 createuser osmistmp -w -S -D -R
 createdb -E UTF8 -O osmistmp osmistmp
 echo "alter user osmistmp encrypted password 'osmistmp';" | psql osmistmp
-psql -d osmistmp < /usr/share/postgresql/8.4/contrib/btree_gist.sql
 
 # Create schema
 echo Creating schema
+psql -d osmistmp < /usr/share/postgresql/8.4/contrib/btree_gist.sql
+#psql -d osmistmp < /home/avar/src/osm.nix.is/osm-applications-utils-osmosis-trunk/script/contrib/apidb_0.6.sql
+
 cd /home/avar/src/osm.nix.is/osm-sites-rails_port
 
 echo "development:"           > config/database.yml
@@ -37,7 +39,10 @@ nice -n 5 rake db:migrate
 
 # Import Iceland.osm
 echo Importing data
-nice -n 5 osmosis --read-xml-0.6 /var/www/osm.nix.is/latest/Iceland.osm.bz2 --write-apidb-0.6 populateCurrentTables=yes host="localhost" database="osmistmp" user="osmistmp" password="osmistmp" validateSchemaVersion=no
+nice -n 5 osmosis \
+    --read-xml-0.6 /var/www/osm.nix.is/latest/Iceland.osm.bz2 \
+    --write-apidb-0.6 \
+    populateCurrentTables=yes host="localhost" database="osmistmp" user="osmistmp" password="osmistmp" validateSchemaVersion=no
 
 ## Rename it & delete
 

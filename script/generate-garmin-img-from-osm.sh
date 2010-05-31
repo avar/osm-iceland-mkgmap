df0fa5eb7edfe31bf449eb36948acbeb44c22bd7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-31 18:20:35 +0000

    Long command moved from v's crontab

---
+++ script/generate-garmin-img-from-osm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+perl /var/www/osm.nix.is/script/generate-garmin-img-from-osm \
+    --date=$(date --iso-8601) \
+    --osm "/var/www/osm.nix.is/dump/$(date --iso-8601).osm.bz2" \
+    --min-osm-size=2000000 \
+    --mkgmap-dir=/home/avar/src/osm.nix.is/mkgmap \
+    --mapname 13314530 \
+    --description="Iceland OSM" \
+    --country-name=ICELAND \
+    --country-abbr=ICE \
+    --out-dir /var/www/osm.nix.is \
+    --out-file=Iceland.osm \
+    --osm2mp-dir=/home/avar/src/osm.nix.is/osm2mp \
+    --out-mp=Iceland.mp 2>&1 \
+    | ack -v '^SEVERE \(RoadNetwork\)'

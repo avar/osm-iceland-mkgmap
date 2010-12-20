fccb261f498d2046c8085310fc3cdfec9089ffb2
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-12-20 21:57:16 +0000

    do --mkgmap-path instead of --mkgmap-dir so I can use this with debian
    Also comment out the mkgmap version thingy,

--- script/generate-garmin-img-from-osm.sh
+++ script/generate-garmin-img-from-osm.sh
@@ -4,7 +4,7 @@ perl /var/www/osm.nix.is/script/generate-garmin-img-from-osm \
     --date=$(date --iso-8601) \
     --osm "/var/www/osm.nix.is/dump/$(date --iso-8601).osm.bz2" \
     --min-osm-size=2000000 \
-    --mkgmap-dir=/home/avar/src/osm.nix.is/mkgmap \
+    --mkgmap-path=/usr/share/mkgmap/mkgmap.jar \
     --mapname 13314530 \
     --description="Iceland OSM" \
     --country-name=ICELAND \
